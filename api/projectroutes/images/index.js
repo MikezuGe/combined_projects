a2db41af7974dee059a35975260cdc82404daf4a
MikezuGe <MikezuGe@users.noreply.github.com>
2018-04-26 21:56:39 +0300

    Image thumbnail srv & ui sides

--- api/projectroutes/images/index.js
+++ api/projectroutes/images/index.js
@@ -1,15 +1,47 @@
 const express = require('express');
 const fs = require('fs');
 const router = express.Router();
+const sharp = require('sharp');
 
 // Custom router imports
 // ...
 
+
+const projectRoot = './public/images/';
+const dataRoot = './public/images/data/';
+const thumbnailRoot = './public/images/data/thumbnails/';
+
 router
 
 .get('/api/images', (req, res) => {
-  fs.readdir('./public/images/data', (err, images) => {
-    res.send(images);
+  fs.readdir(`${dataRoot}`, (err, fileNames) => {
+    const filteredFileNames = fileNames.filter(fileName => fs.lstatSync(`${dataRoot}${fileName}`).isFile())
+    Promise.all(filteredFileNames
+      .map(fileName => new Promise((resolve, reject) => {
+      fs.readFile(`${dataRoot}${fileName}`, (err, file) => {
+        const image = sharp(file);
+        image
+          .metadata()
+          .then(metadata => {
+            image
+              .resize(Math.trunc(metadata.width / metadata.height * 128), 128)
+              .toFile(`${thumbnailRoot}${fileName}`)
+              .then(data => {
+                resolve(true);
+              })
+              .catch(err => {
+                reject('Thumbnails were not created');
+              });
+          });
+      });
+    })))
+      .then(() => {
+        res.send({
+          thumbnailUrls: filteredFileNames.map(fileName => `${'data/thumbnails/'}${fileName}`),
+          imageUrls: filteredFileNames.map(fileName => `${'data/'}${fileName}`),
+        });
+      })
+      .catch(err => console.log(err));
   });
 });
 
--- public/images/src/app.js
+++ public/images/src/app.js
@@ -1,27 +1,64 @@
 import axios from 'axios';
-
-
-const parser = (images, location) => images.map(src => {
-  const container = document.createElement('div');
-  container.className = 'imagecontainer';
-  const image = document.createElement('img');
-  image.className = 'image';
-  container.appendChild(image);
-  image.src = `${location}data/${src}`;
-  return container;
-});
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import './css/style.css';
 
 
 axios.defaults.baseURL = `${window.location.origin}/api`;
 
-axios.get('/images')
-  .then(res => {
-    const images = res.data;
-    const root = document.getElementById('root');
-    parser(images, window.location).forEach(element => {
-      root.appendChild(element);
+
+class App extends Component {
+
+  state = {
+    thumbnailUrls: [],
+    imageUrls: [],
+    displayedImage: '',
+  }
+
+  componentDidMount() {
+    axios.get('/images')
+      .then(result => {
+        const { location, } = window;
+        const { thumbnailUrls, imageUrls, } = result.data;
+        this.setState({
+          imageUrls: imageUrls.map(url => `${location}${url}`),
+          thumbnailUrls: thumbnailUrls.map(url => `${location}${url}`),
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  openImage = index => {
+    console.log(index);
+    console.log(this.state.imageUrls[index]);
+    this.setState({
+      displayedImage: this.state.imageUrls[index],
     });
-  })
-  .catch(err => {
-    console.log(err);
-  });
+  }
+
+  closeImage = () => {
+    this.setState({
+      displayedImage: '',
+    })
+  }
+
+  render() {
+    console.log(this.state);
+    return <div className={'container'}>
+      { this.state.thumbnailUrls.length > 0 && this.state.thumbnailUrls.map((url, index) => <img className={'image'} key={index} onClick={() => { this.openImage(index) }} src={url} />) }
+      { this.state.displayedImage && bigImage(this.state.displayedImage, this.closeImage) }
+    </div>
+  }
+
+}
+
+
+const bigImage = (url, close) => <img onClick={close} src={url} />;
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);

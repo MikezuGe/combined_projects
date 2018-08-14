00578001e88b293e49e70c182e14927a3191e3fc
MikezuGe <MikezuGe@users.noreply.github.com>
2018-08-14 21:59:43 +0300

    Scrapped yourbudget, installed graphql

--- public/yourbudget/src/layout/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Menu, } from './Menu';
-export { default as Header, } from './Header';
-export { default as SideMenu, } from './Sidemenu';
-export { default as ActionModal, } from './Actionmodal';
--- public/yourbudget/src/utility/firstLetterToUpper.js
+++
@@ -1,5 +0,0 @@
-
-const firstLetterToUpper = word => word.charAt(0).toUpperCase() + word.slice(1);
-
-
-export default firstLetterToUpper;

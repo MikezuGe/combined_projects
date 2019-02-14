2e9ba357657ae74ca6c0bd5b515f2f92acf48af9
MikezuGe <MikezuGe@users.noreply.github.com>
2019-02-14 08:49:32 +0200

    Work on searchbar and tags component

--- public/shared_assets/components/atoms/Tag/index.js
+++ public/shared_assets/components/atoms/Tag/index.js
@@ -1,12 +1,105 @@
-import React from 'react';
+import React, { useState, useEffect, } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import { Icon, } from '..';
 
 
-const Tag = () => {
+const Wrapper = styled.span`
+background: lightgray;
+height: 2em;
+line-height: 2em;
+min-width: 3em;
+${({ removeClick, }) => `
+border-radius: ${removeClick ? '1em 0 0 1em' : '1em'};
+margin: ${removeClick ? '0 0 0 0.5em' : '0 0.5em'};
+&:first-child { margin: ${removeClick ? '0' : '0 0.5em 0 0'}; }
+&:last-child { margin: ${removeClick ? '0' : '0 0 0 0.5em'}; }
+padding: ${removeClick ? '0 0.5em 0 1em' : '0 1em'};
+`}
+`;
+
+const StyledInput = styled.input`
+border: none;
+`;
+
+const StyledIcon = styled(Icon)`
+background: lightgray;
+border-left: 1px solid gray;
+border-radius: 0 1em 1em 0;
+height: 2em;
+width: 2em;
+`;
+
+
+const Tag = ({ name, value: defaultValue, onClick, onChangeDone, removeClick, }) => {
+  const [ value, setValue, ] = useState(defaultValue);
+  const [ editing, setEditing, ] = useState(!!onChangeDone);
+
+  const handleEditDone = () => {
+    value
+      ? onChangeDone(value)
+      : removeClick();
+    setEditing(false);
+  };
+
+  useEffect(() => {
+    if (!editing) {
+      return;
+    }
+    const listener = ({ key, keyCode, }) => {
+      ((key === 'Enter' || keyCode === 13) && handleEditDone())
+      || ((key === 'Escape' || keyCode === 27) && removeClick());
+    };
+    document.addEventListener('keyup', listener);
+    return () => document.removeEventListener('keyup', listener);
+  }, [ editing, value, ]);
+
   return (
-    <div>
-      {'Tag'}
-    </div>
-  )
+    <React.Fragment>
+      <Wrapper
+        onClick={onClick ? onClick : !editing ? (() => setEditing(true)) : undefined}
+        removeClick={!!removeClick}
+      >
+        <span>
+          {`${name}${editing || value ? ': ' : ''}`}
+        </span>
+        {editing
+          ? (
+            <StyledInput
+              autoFocus
+              value={value}
+              onChange={e => setValue(e.target.value)}
+              onBlur={handleEditDone}
+            />
+          )
+          : (
+            <span>
+              {value}
+            </span>
+          )}
+      </Wrapper>
+      {removeClick && (
+        <StyledIcon
+          size={16}
+          icon={'clear'}
+          onClick={removeClick}
+        />
+      )}
+    </React.Fragment>
+  );
+};
+
+Tag.defaultProps = {
+  value: '',
+};
+
+Tag.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onClick: PropTypes.func,
+  onChangeDone: PropTypes.func,
+  removeClick: PropTypes.func,
 };
 
 
--- public/shared_assets/components/molecules/Searchbar/index.js
+++ public/shared_assets/components/molecules/Searchbar/index.js
@@ -1,14 +1,93 @@
-import React from 'react';
+import React, { useState, } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-import { Tag, } from '../../atoms';
+import { Icon, Tag, } from '../../atoms';
 
 
-const Searchbar = () => {
+const Bar = styled.div`
+align-items: center;
+background: ${({ theme, }) => theme.secondaryColor}
+border-radius: 1.5em;
+display: flex;
+height: 3em;
+margin-bottom: 0.5em;
+padding: 0 1em;
+`;
+
+const StyledIcon = styled(Icon)`
+margin-right: 0.5em;
+`;
+
+
+const handleValueByType = (type, value) =>
+  type === 'date'
+    ? new Date(value)
+    : type === 'number'
+      ? parseFloat(value)
+      : value;
+
+const tagsReducer = (total, { key, type, value, }) => (total[key] = handleValueByType(type, value), total);
+
+
+const Searchbar = ({ filters, onFiltersChange, }) => {
+  const [ tags, setTags, ] = useState([]);
+
+  const handleTagChangeDone = (value, i) => setTags(prevTags => {
+    prevTags[i] = { ...prevTags[i], value, };
+    onFiltersChange(prevTags.reduce(tagsReducer, {}));
+    return prevTags;
+  });
+  const handleTagRemoveClick = i => setTags(prevTags => {
+    prevTags.splice(i, 1);
+    onFiltersChange(prevTags.reduce(tagsReducer, {}));
+    return prevTags;
+  });
+
   return (
-    <div>
-      <Tag />
-    </div>
-  )
+    <React.Fragment>
+
+
+      <Bar>
+        <StyledIcon icon={'search'} />
+        {tags.map(({ key, ...tag }, i) => (
+          <Tag
+            key={key}
+            {...tag}
+            onChangeDone={value => handleTagChangeDone(value, i)}
+            removeClick={() => handleTagRemoveClick(i)}
+          />
+        ))}
+      </Bar>
+
+
+      <Bar>
+        {filters
+          .filter(({ key, }) => !tags.find(tag => key === tag.key))
+          .map(filter => (
+            <Tag
+              key={filter.key}
+              name={filter.name}
+              onClick={() => setTags(prevTags => [ ...prevTags, filter, ])}
+            />
+          ))
+        }
+      </Bar>
+
+
+    </React.Fragment>
+  );
+};
+
+Searchbar.propTypes = {
+  filters: PropTypes.arrayOf(
+    PropTypes.shape({
+      key: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+  onFiltersChange: PropTypes.func.isRequired,
 };
 
 

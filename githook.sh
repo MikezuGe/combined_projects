f8de33e0c582106b93ec787c1a3986d8aac4e4a4
MikezuGe <MikezuGe@users.noreply.github.com>
2019-02-25 18:56:26 +0200

    Githook updated to install with yarn

--- githook.sh
+++ githook.sh
@@ -6,7 +6,7 @@ git add .
 git reset --hard
 git pull
 
-npm install --only=production
+yarn install --production --pure-lockfile
 
 ps -aux | grep [n]ode | awk '{print $2}' | xargs kill
 nohup node index.js &> /var/log/nodesrv.log &

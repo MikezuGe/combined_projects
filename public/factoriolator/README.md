32c8af76226428296c9c6f0d3100022f8ce8e692
MikezuGe <MikezuGe@users.noreply.github.com>
2019-06-07 16:23:33 +0300

    Removed project: Factoriolator

--- public/factoriolator/README.md
+++
@@ -1,1 +0,0 @@
-Add pollution to everything
--- public/factoriolator/src/constants/index.js
+++
@@ -1,6 +0,0 @@
-export { default as ASSEMBLERS, } from './assemblers';
-export {
-  default as MODULES,
-  MODULE_TYPES,
-} from './modules';
-export { default as RECIPES, } from './recipes';
